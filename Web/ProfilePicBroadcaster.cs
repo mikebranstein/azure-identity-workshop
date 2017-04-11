c5d6e0fb1ea9ee0014d0a0b7b4eba48f1232e306
Mike Branstein <mike.branstein@gmail.com>
2017-04-10 22:16:01 -0400

    add signalr hub, broadcaster, and js listener

---
+++ Web/ProfilePicBroadcaster.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    /// <summary>
+    ///  FROM https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/tutorial-server-broadcast-with-signalr
+    /// </summary>
+    public class ProfilePicBroadcaster
+    {
+        // Singleton instance
+        private readonly static Lazy<ProfilePicBroadcaster> _instance = new Lazy<ProfilePicBroadcaster>(() => new ProfilePicBroadcaster(GlobalHost.ConnectionManager.GetHubContext<ProfilePicHub>().Clients));
+
+        private ProfilePicBroadcaster(IHubConnectionContext<dynamic> clients)
+        {
+            Clients = clients;
+        }
+
+        public static ProfilePicBroadcaster Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        private IHubConnectionContext<dynamic> Clients
+        {
+            get;
+            set;
+        }
+
+        public void BroadcastUpdatedProfilePic(string profilePicUrl)
+        {
+            Clients.All.profilePicUpdated(profilePicUrl);
+        }
+    }
+}
---
+++ Web/ProfilePicHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Web
+{
+    public class ProfilePicHub : Hub
+    {
+        private readonly ProfilePicBroadcaster _profilePicture;
+
+        public ProfilePicHub(ProfilePicBroadcaster profilePicture)
+        {
+            _profilePicture = profilePicture;
+        }
+
+    }
+}
